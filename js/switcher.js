38054d76b3312b4dd2b9ee9e1322faa854e1ac30
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-07-08 13:40:41 +0300

    #bug fixed

--- js/switcher.js
+++ js/switcher.js
@@ -13,7 +13,37 @@ $('#b1000').click(() => {
     $('#prime-prod').hide();
 })
 
+$('#b1000-2').click(() => {
+    $("#1000").show();
+    $('#1000-prod').show();
+    $("#2000").hide();
+    $('#2000-prod').hide();
+    $("#3000").hide();
+    $('#3000-prod').hide();
+    $("#4000").hide();
+    $('#4000-prod').hide();
+    $("#ultra").hide();
+    $('#ultra-prod').hide();
+    $("#prime").hide();
+    $('#prime-prod').hide();
+})
+
 $('#b2000').click(() => {
+    $("#2000").show();
+    $('#2000-prod').show();
+    $("#1000").hide();
+    $('#1000-prod').hide();
+    $("#3000").hide();
+    $('#3000-prod').hide();
+    $("#4000").hide();
+    $('#4000-prod').hide();
+    $("#ultra").hide();
+    $('#ultra-prod').hide();
+    $("#prime").hide();
+    $('#prime-prod').hide();
+})
+
+$('#b2000-2').click(() => {
     $("#2000").show();
     $('#2000-prod').show();
     $("#1000").hide();
@@ -43,6 +73,21 @@ $('#b3000').click(() => {
     $('#prime-prod').hide();
 })
 
+$('#b3000-2').click(() => {
+    $("#3000").show();
+    $('#3000-prod').show();
+    $("#2000").hide();
+    $('#2000-prod').hide();
+    $("#1000").hide();
+    $('#1000-prod').hide();
+    $("#4000").hide();
+    $('#4000-prod').hide();
+    $("#ultra").hide();
+    $('#ultra-prod').hide();
+    $("#prime").hide();
+    $('#prime-prod').hide();
+})
+
 $('#b4000').click(() => {
     $("#4000").show();
     $('#4000-prod').show();
@@ -57,6 +102,22 @@ $('#b4000').click(() => {
     $("#prime").hide();
     $('#prime-prod').hide();
 })
+
+$('#b4000-2').click(() => {
+    $("#4000").show();
+    $('#4000-prod').show();
+    $("#2000").hide();
+    $('#2000-prod').hide();
+    $("#3000").hide();
+    $('#3000-prod').hide();
+    $("#1000").hide();
+    $('#1000-prod').hide();
+    $("#ultra").hide();
+    $('#ultra-prod').hide();
+    $("#prime").hide();
+    $('#prime-prod').hide();
+})
+
 
 $('#bultra').click(() => {
     $("#ultra").show();
@@ -73,7 +134,37 @@ $('#bultra').click(() => {
     $('#prime-prod').hide();
 })
 
+$('#bultra-2').click(() => {
+    $("#ultra").show();
+    $('#ultra-prod').show();
+    $("#2000").hide();
+    $('#2000-prod').hide();
+    $("#3000").hide();
+    $('#3000-prod').hide();
+    $("#4000").hide();
+    $('#4000-prod').hide();
+    $("#1000").hide();
+    $('#1000-prod').hide();
+    $("#prime").hide();
+    $('#prime-prod').hide();
+})
+
 $('#bprime').click(() => {
+    $("#prime").show();
+    $('#prime-prod').show();
+    $("#2000").hide();
+    $('#2000-prod').hide();
+    $("#3000").hide();
+    $('#3000-prod').hide();
+    $("#4000").hide();
+    $('#4000-prod').hide();
+    $("#ultra").hide();
+    $('#ultra-prod').hide();
+    $("#1000").hide();
+    $('#1000-prod').hide();
+})
+
+$('#bprime-2').click(() => {
     $("#prime").show();
     $('#prime-prod').show();
     $("#2000").hide();
